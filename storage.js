dc7250b2532afe9f9f618ae39b69eb92ba61deb5
bezrukov <bezrukov-yra@mail.ru>
2020-04-28 21:49:32 +0300

    comment unit-test

--- storage.js
+++ storage.js
@@ -19,15 +19,13 @@ function storageGetAll() {
   }
 }
 
-//unit-tests
-function unitTest() {
-  mockData = [{id: 1, name: 'my-name'}];
-  storageAdd(mockData);
-  newMockData = JSON.parse(localStorage.getItem('storage-items'));
+// //unit-tests
+// function unitTest() {
+//   mockData = [{id: 1, name: 'my-name'}];
+//   storageAdd(mockData);
+//   newMockData = JSON.parse(localStorage.getItem('storage-items'));
+//   let condition = newMockData[0].id === mockData[0].id && newMockData[0].name === mockData[0].name;
+//   console.assert(condition, 'function storageAdd not work');
+// }
 
-  let condition = newMockData[0].id === mockData[0].id && newMockData[0].name === mockData[0].name;
-
-  console.assert(condition, 'function storageAdd not work');
-}
-
-unitTest();
+// unitTest();
